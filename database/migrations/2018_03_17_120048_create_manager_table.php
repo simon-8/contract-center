9a77c0e20e5ef87f87bfe08b3dd1ab48b9d66993
simon8 <liu@simon8.com>
2018-05-09 16:56:44 +0800

    添加RABC所需表/model

--- database/migrations/2018_03_17_120048_create_manager_table.php
+++ database/migrations/2018_03_17_120048_create_manager_table.php
@@ -17,11 +17,12 @@ class CreateManagerTable extends Migration
             $table->increments('id');
             $table->string('username',50)->comment('用户名');
             $table->string('password',255)->comment('密码');
+            $table->unsignedTinyInteger('groupid')->comment('会员组ID');
             $table->string('truename',50)->comment('真实姓名')->default('');
             $table->string('email',100)->comment('邮箱号码')->default('');
             $table->unsignedTinyInteger('is_admin')->comment('是否管理员')->default(0);
             $table->string('avatar')->comment('头像')->default('');
-            $table->string('role')->comment('权限列表')->default('');
+            //$table->string('role')->comment('权限列表')->default('');
             //$table->string('salt',4)->comment('密码加盐');
             $table->string('lastip')->comment('上一次登录ip')->default('');
             $table->timestamp('lasttime')->comment('最后登录时间');
