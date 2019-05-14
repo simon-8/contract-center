326af4791cd7045f35bb61ca2e83aa9c34f78baf
simon <651138009@qq.com>
2019-05-14 23:09:04 +0800

    重写中间件authenticate方法, 实现自定义跳转

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,20 +2,51 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
+    protected $guards = null;
+
     /**
-     * Get the path the user should be redirected to when they are not authenticated.
+     * 重写认证方法
+     * @param \Illuminate\Http\Request $request
+     * @param array $guards
+     * @throws AuthenticationException
+     */
+    protected function authenticate($request, array $guards)
+    {
+        if (empty($guards)) {
+            $guards = [null];
+        }
+
+        $this->guards = $guards;
+
+        foreach ($guards as $guard) {
+            if ($this->auth->guard($guard)->check()) {
+                return $this->auth->shouldUse($guard);
+            }
+        }
+
+        throw new AuthenticationException(
+            'Unauthenticated.', $guards, $this->redirectTo($request)
+        );
+    }
+    /**
+     * 重写跳转方法
      *
      * @param  \Illuminate\Http\Request  $request
      * @return string
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        $guard = array_shift($this->guards);
+        if ($guard === 'admin') {
+            if (! $request->expectsJson()) {
+                return route('admin.login');
+            }
         }
+
     }
 }
