6628005bbd6d00cc8ce46c170f62436863e54e10
simon <liu@simon8.com>
2020-04-28 14:55:54 +0800

    wkhtmltopdf-admd64 增加默认配置;

--- config/snappy.php
+++ config/snappy.php
@@ -7,7 +7,10 @@ return array(
         'enabled' => true,
         'binary'  => '/usr/local/bin/wkhtmltopdf-amd64',
         'timeout' => false,
-        'options' => array(),
+        'options' => [
+            'margin-top' => 25.4,
+            'margin-bottom' => 19.1,
+        ],
         'env'     => array(),
     ),
     'image' => array(
