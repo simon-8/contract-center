97618eee5adf4cb2b9c0b338a33fe9a7ec44bee6
simon <651138009@qq.com>
2020-09-17 07:28:14 +0800

    新增测试代码

---
+++ app/Services/EsignFaceService.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Note: *
+ * User: Liu
+ * Date: 2020/9/16
+ * Time: 21:39
+ */
+namespace App\Services;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Cache;
+
+class EsignFaceService
+{
+    const API_DOMAIN = 'https://smlopenapi.esign.cn';// 测试
+    //const API_DOMAIN = 'https://openapi.esign.cn';// 正式
+
+    protected function requestHeader()
+    {
+        return [
+            'X-Tsign-Open-App-Id' => config('esign.appid'),
+            'X-Tsign-Open-Token' => $this->getToken(),
+            'Content-Type' => 'application/json',
+        ];
+    }
+
+    protected function requestGet($api, $data)
+    {
+        if (!empty($data) && is_array($data)) {
+            $data = http_build_query($data);
+        }
+        $api = self::API_DOMAIN. $api;
+        $ch = curl_init($api . '?' . $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+        if (!empty($_SERVER['HTTP_USER_AGENT'])) curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        //curl_setopt($ch, CURLOPT_POST, TRUE);
+        //curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->requestHeader());
+        $response = curl_exec($ch);
+        info($response);
+        $error = curl_error($ch);
+        curl_close($ch);
+        if (empty($error)) {
+            return $response;
+        }
+        throw new \Exception($error);
+    }
+
+    protected function requestPost($api, $data)
+    {
+        //if (!empty($data) && is_array($data)) {
+        //    $data = http_build_query($data);
+        //}
+        $api = self::API_DOMAIN. $api;
+        $ch = curl_init($api);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+        if (!empty($_SERVER['HTTP_USER_AGENT'])) curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->requestHeader());
+        $response = curl_exec($ch);
+        info($response);
+        $error = curl_error($ch);
+        curl_close($ch);
+        if (empty($error)) {
+            return $response;
+        }
+        throw new \Exception($error);
+    }
+
+    protected function getToken()
+    {
+        $token = Cache::get('esign:v2:token');
+        if ($token) return $token;
+
+        $url = '/v1/oauth2/access_token';
+        $response = $this->requestGet($url, [
+            'appId' => config('esign.appid'),
+            'secret' => config('esign.appSecret'),
+            'grantType' => 'client_credentials',
+        ]);
+        $response = json_decode($response, true);
+        if ($response['code']) throw new \Exception($response['message']);
+        $token = $response['data']['token'];
+        $expireMs = $response['data']['expiresIn'];// 毫秒
+        Cache::put('esign:v2:token', $token, Carbon::createFromTimestampMs($expireMs));
+        return $token;
+    }
+
+    public function getFaceUrl($accountId)
+    {
+        $url = "/v2/identity/auth/web/{$accountId}/indivIdentityUrl";
+        $response = $this->requestPost($url, [
+            'accountId' => $accountId,
+            'authType' => 'PSN_FACEAUTH_BYURL',
+            'availableAuthTypes' => 'PSN_FACEAUTH_BYURL',
+            //'receiveUrlMobileNo',
+            //'contextInfo',
+            //'authType',
+        ]);
+        $response = json_decode($response, true);
+        if ($response['code']) throw new \Exception($response['message']);
+        return $response['data']['shortLink'];
+    }
+}
--- tests/Unit/EsignTest.php
+++ tests/Unit/EsignTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Services\EsignFaceService;
 use App\Services\EsignService;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -9,14 +10,18 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class EsignTest extends TestCase
 {
-    public function testIndex(EsignService $service)
+    public function testIndex()
     {
+        $service = new EsignFaceService();
         $url = $service->getFaceUrl('A61B017D62AD4D0AB76AB29C85D3D2A8');
-        $url = '/api/banner/1';
-        $response = $this->get($url);
-        $response->assertOk();
-        $data = json_decode($response->getContent(), true);
-        $data = $data['data'];
-        $this->assertArrayHasKey('id', array_shift($data));
+        info(__METHOD__);
+        //info($url);
+        //$this->assert
+        //$url = '/api/banner/1';
+        //$response = $this->get($url);
+        //$response->assertOk();
+        //$data = json_decode($response->getContent(), true);
+        //$data = $data['data'];
+        //$this->assertArrayHasKey('id', array_shift($data));
     }
 }
